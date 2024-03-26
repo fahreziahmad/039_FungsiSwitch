1d286576ee328ea3ca7ae300936c775a04b3187a
fahreziahmad <alfahreziahmadsyahyana@gmail.com>
2024-03-26 17:55:51 +0700

    Add project files.

---
+++ FungsiSwitch/FungsiSwitch.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int bilA, bilB;
+
+void input()
+{
+    cout << "Masukkan bilangan pertama = ";
+    cin >> bilA;
+    cout << "Masukkan bilangan kedua = ";
+    cin >> bilB;
+}
