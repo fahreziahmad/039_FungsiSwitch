13653fcf3be1be106ba990a3da860a2e841bae99
fahreziahmad <alfahreziahmadsyahyana@gmail.com>
2024-03-26 18:03:56 +0700

    menambahkan int main

--- FungsiSwitch/FungsiSwitch.cpp
+++ FungsiSwitch/FungsiSwitch.cpp
@@ -27,3 +27,21 @@ int pembagian(float a, float b)
     return a / b;
 }
 
+int main()
+{
+    int pilihan;
+    do
+    {
+        cout << "Kalkulator Sederhana" << endl;
+        cout << "====================" << endl;
+        cout << "1. Penjumlahan" << endl;
+        cout << "2. Pengurangan" << endl;
+        cout << "3. Perkalian" << endl;
+        cout << "4. Pembagian" << endl;
+        cout << "5. keluar" << endl;
+        cout << "=====================" << endl;
+        cout << "Masukkan menu pilihan : ";
+        cin >> pilihan;
+
+        switch (pilihan)
+        {
